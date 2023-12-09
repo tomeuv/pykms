78b32290fa49311906c8d288e324e5f7e24c53a6
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2023-12-09 11:20:27 +0200

    reorder

--- kms/card.py
+++ kms/card.py
@@ -315,6 +315,39 @@ class Connector(DrmPropObject):
         return [self.card.get_encoder(eid) for eid in self.encoder_ids]
 
 
+class Encoder(DrmObject):
+    def __init__(self, card: Card, id, idx) -> None:
+        super().__init__(card, id, kms.uapi.DRM_MODE_OBJECT_ENCODER, idx)
+
+        res = kms.uapi.drm_mode_get_encoder()
+
+        res.encoder_id = id
+
+        fcntl.ioctl(card.fd, kms.uapi.DRM_IOCTL_MODE_GETENCODER, res, True)
+
+        self.encoder_res = res
+
+        #print(f"encoder {id}: type: {res.encoder_type}")
+
+    def __repr__(self) -> str:
+        return f'Encoder({self.id})'
+
+    @property
+    def crtc(self):
+        if self.encoder_res.crtc_id:
+            return self.card.get_crtc(self.encoder_res.crtc_id)
+        else:
+            return None
+
+    @property
+    def possible_crtcs(self):
+        return [crtc for crtc in self.card.crtcs if self.encoder_res.possible_crtcs & (1 << crtc.idx)]
+
+    @property
+    def encoder_type(self):
+        return kms.EncoderType(self.encoder_res.encoder_type)
+
+
 class Crtc(DrmPropObject):
     def __init__(self, card: Card, id, idx) -> None:
         super().__init__(card, id, kms.uapi.DRM_MODE_OBJECT_CRTC, idx)
@@ -365,39 +398,6 @@ class Crtc(DrmPropObject):
         if plane:
             return plane
         raise Exception("No primary plane")
-
-
-class Encoder(DrmObject):
-    def __init__(self, card: Card, id, idx) -> None:
-        super().__init__(card, id, kms.uapi.DRM_MODE_OBJECT_ENCODER, idx)
-
-        res = kms.uapi.drm_mode_get_encoder()
-
-        res.encoder_id = id
-
-        fcntl.ioctl(card.fd, kms.uapi.DRM_IOCTL_MODE_GETENCODER, res, True)
-
-        self.encoder_res = res
-
-        #print(f"encoder {id}: type: {res.encoder_type}")
-
-    def __repr__(self) -> str:
-        return f'Encoder({self.id})'
-
-    @property
-    def crtc(self):
-        if self.encoder_res.crtc_id:
-            return self.card.get_crtc(self.encoder_res.crtc_id)
-        else:
-            return None
-
-    @property
-    def possible_crtcs(self):
-        return [crtc for crtc in self.card.crtcs if self.encoder_res.possible_crtcs & (1 << crtc.idx)]
-
-    @property
-    def encoder_type(self):
-        return kms.EncoderType(self.encoder_res.encoder_type)
 
 
 class Plane(DrmPropObject):
