fb884459215fbe107ec85abe083e23562a4e467b
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2024-08-22 11:51:30 +0300

    draw-perf-test: numpy test

--- utils/draw-perf-test.py
+++ utils/draw-perf-test.py
@@ -26,28 +26,27 @@ class State:
         self.framenum = 0
         self.bar_step = 4
 
-def init_fbs_1(state: State):
+        self.test_mode = 0
+
+# Test with direct mmap access
+
+def init_fbs_0(state: State):
     mmaps = []
-    numpybufs = []
 
     for fb in state.fbs:
         fb_mmaps = fb.mmap()
         buf = fb_mmaps[0]
         buf[:] = bytearray(fb.planes[0].size)
 
-        b = np.frombuffer(buf, dtype=np.uint32).reshape(fb.height, fb.width)
-
         mmaps.append(fb_mmaps)
-        numpybufs.append(b)
 
     state.custom_state = {
-        'numpybufs': numpybufs,
         'mmaps': mmaps,
         'line_0': bytes([0] * (state.mode.hdisplay * 4)),
         'line_1': bytes([0xff] * (state.mode.hdisplay * 4)),
     }
 
-def draw_fb_1(state: State, fb_idx: int, old_y):
+def draw_fb_0(state: State, fb_idx: int, old_y):
     fb = state.fbs[fb_idx]
 
     m = state.custom_state['mmaps'][fb_idx][0]
@@ -56,9 +55,47 @@ def draw_fb_1(state: State, fb_idx: int, old_y):
     m[old_y * pitch:old_y * pitch + fb.width * 4] = state.custom_state['line_0']
     m[state.bar_y * pitch:state.bar_y * pitch + fb.width * 4] = state.custom_state['line_1']
 
-    #b = numpybufs[old_fb]
-    #b[old_y, :] = 0
-    #b[bar_y, :] = 0xffffff
+
+# Test with mmap + numpy
+
+def init_fbs_1(state: State):
+    numpybufs = []
+
+    for fb in state.fbs:
+        fb_mmaps = fb.mmap()
+        buf = fb_mmaps[0]
+
+        b = np.frombuffer(buf, dtype=np.uint32).reshape(fb.height, fb.width)
+
+        b[:] = 0x000000
+
+        numpybufs.append(b)
+
+    state.custom_state = {
+        'numpybufs': numpybufs,
+    }
+
+def draw_fb_1(state: State, fb_idx: int, old_y):
+    b = state.custom_state['numpybufs'][fb_idx]
+    b[old_y, :] = 0
+    b[state.bar_y, :] = 0xffffff
+
+
+def init_fbs(state: State):
+    if state.test_mode == 0:
+        init_fbs_0(state)
+    elif state.test_mode == 1:
+        init_fbs_1(state)
+    else:
+        raise RuntimeError()
+
+def draw_fb(state: State, fb_idx: int, old_y):
+    if state.test_mode == 0:
+        draw_fb_0(state, fb_idx, old_y)
+    elif state.test_mode == 1:
+        draw_fb_1(state, fb_idx, old_y)
+    else:
+        raise RuntimeError()
 
 def handle_fps(state: State):
     ts = time.perf_counter()
@@ -96,7 +133,7 @@ def handle_pageflip(state: State):
     if old_fb is not None:
         ts1 = time.perf_counter()
 
-        draw_fb_1(state, old_fb, old_y)
+        draw_fb(state, old_fb, old_y)
 
         ts2 = time.perf_counter()
 
@@ -108,7 +145,8 @@ def handle_pageflip(state: State):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--connector", default="")
+    parser.add_argument('-c', '--connector', default='')
+    parser.add_argument('-m', '--mode', type=int, default=0)
     args = parser.parse_args()
 
     card = kms.Card()
@@ -120,12 +158,13 @@ def main():
     mode = conn.get_default_mode()
 
     state = State(card, conn, crtc, plane, mode)
+    state.test_mode = args.mode
 
     for _ in range(3):
         fb = kms.DumbFramebuffer(card, mode.hdisplay, mode.vdisplay, kms.PixelFormats.XRGB8888)
         state.fbs.append(fb)
 
-    init_fbs_1(state)
+    init_fbs(state)
 
     card.disable_planes()
 
