5065c65103224e48a263fe86f94fa22b2cce7641
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2024-08-22 13:39:14 +0300

    Add test_install.py

---
+++ tests/test_install.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import unittest
+import kms
+
+class TestInstall(unittest.TestCase):
+    def test_install(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
